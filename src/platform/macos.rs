f707d2de9239d15f737fc21a4d210430cd5df07b
bmmh1 <brengels.mendoza@me.com>
2026-07-09 23:20:29 -0500

    Update macos.rs

--- src/platform/macos.rs
+++ src/platform/macos.rs
@@ -992,11 +992,12 @@ rm -rf {tmp_dir}
         .and_then(|mut f| f.write_all(script.as_bytes()))?;
    }
    let _ = Command::new("chmod").args(&["+x", &script_path]).status();
-   Command::new("nohup")
-    .args(&["/bin/bash", &script_path])
+   Command::new("/bin/bash")
+    .arg(&script_path)
     .stdin(Stdio::null())
     .stdout(Stdio::null())
     .stderr(Stdio::null())
+    .process_group(0)
     .spawn()?;
 
     log::info!("[root-update] Update script launched.");
