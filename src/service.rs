e2d0dde7196fb422fccc548115d8d2c7f932c6b9
bmmh1 <brengels.mendoza@me.com>
2026-07-16 09:56:16 -0500

    Update service.rs

--- src/service.rs
+++ src/service.rs
@@ -7,11 +7,10 @@ fn main() {}
 fn main() {
     let args: Vec<String> = std::env::args().collect();
     if args.len() > 1 && args[1] == "--write-plists" {
-        hbb_common::init_log(false, "service");
         if let Err(e) = librustdesk::platform::write_plists() {
             eprintln!("Failed to write plists: {}", e);
         }
-        return;
+        std::process::exit(0);
     }
     crate::common::load_custom_client();
     hbb_common::init_log(false, "service");
