792a49ba7c72f67587d000886470b7f59096e1a9
bmmh1 <brengels.mendoza@me.com>
2026-07-16 04:11:21 -0500

    Update service.rs

--- src/service.rs
+++ src/service.rs
@@ -5,6 +5,14 @@ fn main() {}
 
 #[cfg(target_os = "macos")]
 fn main() {
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() > 1 && args[1] == "--write-plists" {
+        hbb_common::init_log(false, "service");
+        if let Err(e) = librustdesk::platform::write_plists() {
+            eprintln!("Failed to write plists: {}", e);
+        }
+        return;
+    }
     crate::common::load_custom_client();
     hbb_common::init_log(false, "service");
     crate::start_os_service();
